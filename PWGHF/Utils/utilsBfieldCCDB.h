a12bff245b2fd6a67a0e65b97635f669654e032d
Vít Kučera <vit.kucera@cern.ch>
2024-03-19 14:38:30 +0100

    Simplify initCCDB

--- PWGHF/Utils/utilsBfieldCCDB.h
+++ PWGHF/Utils/utilsBfieldCCDB.h
@@ -19,41 +19,53 @@
 #include <string> // std::string
 
 #include "CCDB/BasicCCDBManager.h"
+#include "DetectorsBase/MatLayerCylSet.h"
 #include "DataFormatsParameters/GRPMagField.h"
 #include "DataFormatsParameters/GRPObject.h"
 
-/// \brief Sets up the grp object for magnetic field (w/o matCorr for propagation)
-/// \param bc is the bunch crossing
-/// \param mRunNumber is an int with the run umber of the previous iteration. If at the current iteration it changes, then the grp object is updated
-/// \param ccdb is the o2::ccdb::BasicCCDBManager object
-/// \param ccdbPathGrp is the path where the GRP oject is stored
-/// \param lut is a pointer to the o2::base::MatLayerCylSet object
+/// \brief Retreive magnetic field from CCDB and initialise the field in the propagator
+/// \param ccdb BasicCCDBManager object
+/// \param ccdbPathGrp path to the GRP oject
+/// \param timestamp timestamp
+template <typename TGrp, typename TCcdb, typename TPath, typename TTimeStamp>
+static void initField(TCcdb const& ccdb, TPath const& ccdbPathGrp, const TTimeStamp timestamp) {
+  TGrp* grpo = ccdb->template getForTimeStamp<TGrp>(ccdbPathGrp, timestamp);
+  if (!grpo) {
+    LOGF(fatal, "Failed to retrieve GRP object for path %s and timestamp %llu", ccdbPathGrp, timestamp);
+  }
+  o2::base::Propagator::initFieldFromGRP(grpo);
+}
+
+/// \brief Sets up the GRP object for magnetic field (w/o matCorr for propagation)
+/// \param bc bunch crossing
+/// \param runNumber run number of the previous iteration. If at the current iteration it changes, then the GRP object is updated
+/// \param ccdb BasicCCDBManager object
+/// \param ccdbPathGrp path to the GRP oject
+/// \param lut pointer to the MatLayerCylSet object
 /// \param isRun2 tells whether we are analysing Run2 converted data or not (different GRP object type)
-void initCCDB(o2::aod::BCsWithTimestamps::iterator const& bc, int& mRunNumber,
-              o2::framework::Service<o2::ccdb::BasicCCDBManager> const& ccdb, std::string const& ccdbPathGrp, o2::base::MatLayerCylSet* lut,
+template <typename TBc>
+static void initCCDB(TBc const& bc,
+              int& runNumber,
+              o2::framework::Service<o2::ccdb::BasicCCDBManager> const& ccdb,
+              std::string const& ccdbPathGrp,
+              const o2::base::MatLayerCylSet* lut,
               bool isRun2)
 {
-  if (mRunNumber != bc.runNumber()) {
-    LOGF(info, "====== initCCDB function called (isRun2==%d)", isRun2);
-    if (isRun2) { // Run 2 GRP object
-      o2::parameters::GRPObject* grpo = ccdb->getForTimeStamp<o2::parameters::GRPObject>(ccdbPathGrp, bc.timestamp());
-      if (grpo == nullptr) {
-        LOGF(fatal, "Run 2 GRP object (type o2::parameters::GRPObject) is not available in CCDB for run=%d at timestamp=%llu", bc.runNumber(), bc.timestamp());
-      }
-      o2::base::Propagator::initFieldFromGRP(grpo);
-      LOGF(info, "Setting magnetic field to %d kG for run %d from its GRP CCDB object (type o2::parameters::GRPObject)", grpo->getNominalL3Field(), bc.runNumber());
-    } else { // Run 3 GRP object
-      o2::parameters::GRPMagField* grpo = ccdb->getForTimeStamp<o2::parameters::GRPMagField>(ccdbPathGrp, bc.timestamp());
-      if (grpo == nullptr) {
-        LOGF(fatal, "Run 3 GRP object (type o2::parameters::GRPMagField) is not available in CCDB for run=%d at timestamp=%llu", bc.runNumber(), bc.timestamp());
-      }
-      o2::base::Propagator::initFieldFromGRP(grpo);
-      LOGF(info, "Setting magnetic field to current %f A for run %d from its GRP CCDB object (type o2::parameters::GRPMagField)", grpo->getL3Current(), bc.runNumber());
-    }
-    if (lut) {
-      o2::base::Propagator::Instance()->setMatLUT(lut);
-    }
-    mRunNumber = bc.runNumber();
+  if (runNumber == bc.runNumber()) {
+    return;
+  }
+  runNumber = bc.runNumber();
+  LOGF(info, "initCCDB function called (isRun2 = %s) for run %d", isRun2 ? "true" : "false", runNumber);
+  auto timestamp = bc.timestamp();
+  if (isRun2) { // Run 2 GRP object
+    initField<o2::parameters::GRPObject>(ccdb, ccdbPathGrp, timestamp);
+  } else { // Run 3 GRP object
+    initField<o2::parameters::GRPMagField>(ccdb, ccdbPathGrp, timestamp);
+  }
+  auto bz = o2::base::Propagator::Instance()->getNominalBz();
+  LOG(info) << "Retrieved GRP for run " << runNumber << " and timestamp " << timestamp << " with magnetic field of " << bz << " kG";
+  if (lut) {
+    o2::base::Propagator::Instance()->setMatLUT(lut);
   }
 } /// end initCCDB
 
