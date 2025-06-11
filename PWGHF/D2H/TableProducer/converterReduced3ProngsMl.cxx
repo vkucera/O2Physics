511d18b0b22cb65d4198915ac2b88a3929dfbb15
Vít Kučera <vit.kucera@cern.ch>
2025-06-11 14:01:18 +0200

    Include What You Use

--- PWGHF/D2H/TableProducer/converterReduced3ProngsMl.cxx
+++ PWGHF/D2H/TableProducer/converterReduced3ProngsMl.cxx
@@ -14,11 +14,12 @@
 ///
 /// \author Fabrizio Grosa <fabrizio.grosa@cern.ch>, CERN
 
-#include "Framework/runDataProcessing.h"
-#include "Framework/AnalysisTask.h"
-#include "Framework/AnalysisDataModel.h"
-
 #include "PWGHF/D2H/DataModel/ReducedDataModel.h"
+
+#include <Framework/AnalysisHelpers.h>
+#include <Framework/AnalysisTask.h>
+#include <Framework/WorkflowSpec.h>
+#include <Framework/runDataProcessing.h>
 
 using namespace o2;
 using namespace o2::framework;
