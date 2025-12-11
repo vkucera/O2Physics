99697be732b695cd3a150f13fcf4f625c2e5c1c6
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2025-12-11 10:58:57 +0100

    Tmp disable cppcheck categories

--- Scripts/process_linter_log.py
+++ Scripts/process_linter_log.py
@@ -77,7 +77,7 @@ def main():
             LinterSpec.GroupPath: 2,
             LinterSpec.GroupSeverity: 3,
             LinterSpec.GroupCategory: 4,
-            LinterSpec.IgnoredCategories: ["selfInitialization"],
+            LinterSpec.IgnoredCategories: ["selfInitialization", "functionStatic", "unreadVariable", "useStlAlgorithm", "variableScope"],
             LinterSpec.IgnoredSeverities: [],
         },
         Linter.Cpplint: {
