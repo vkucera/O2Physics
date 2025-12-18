38603650f57e8250fea4684e3a8cc6bab3ae8f3a
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2025-12-18 20:31:16 +0100

    Update test script

--- .github/workflows/title-check.py
+++ .github/workflows/title-check.py
@@ -1,7 +1,3 @@
-import os
-import re
-import sys
-
 cases = {
     0: {"title": "PWGJE Trigger ALICE3: Fix scope of tables in Jet.h", "labels": "pwgje,trigger"},
     1: {"title": "PWGHFF: Add centrality: (FT0C as default) bin to the ThnSparse", "labels": "pwghf"},
@@ -15,28 +11,45 @@ cases = {
         "labels": "pwgje,pwghf,infra,trigger",
     },
     5: {"title": "PWGHF/D2H, Cmake: Add centrality (FT0C as default) bin to the ThnSparse", "labels": "pwghf"},
-    6: {"title": "Add centrality (FT0C as default) bin to the ThnSparse", "labels": ""},
+    6: {"title": " Add centrality (FT0C as default) bin to the ThnSparse", "labels": ""},
     7: {"title": "[PWGCF]: Add centrality (FT0C as default) bin to the ThnSparse", "labels": "pwgcf"},
     8: {"title": "[Cmake/C++] Add centrality (FT0C as default) bin to the ThnSparse", "labels": ""},
-    9: {"title": "[Cmake/C--]: Add centrality (FT0C as default) bin to the ThnSparse", "labels": "common"},
+    9: {"title": " [Cmake/C--]: Add centrality (FT0C as default) bin to the ThnSparse", "labels": "common"},
     10: {"title": "[Cmake/C--]: Add centrality (FT0C as default) bin to the ThnSparse", "labels": ""},
+    11: {"title": " [Cmake/C--]: Add centrality (FT0C as default) bin to the ThnSparse", "labels": ""},
+    12: {
+        "title": "PWGHF, Cmake Add centrality (FT0C as default) bin to the ThnSparse",
+        "labels": "pwgje,pwghf,trigger",
+    },
+    13: {"title": " PWGJE Trigger  :  Fix scope of tables in Jet.h ", "labels": "pwgje,trigger"},
+    14: {
+        "title": " PWGHF, Cmake Add centrality (FT0C as default) bin to the ThnSparse",
+        "labels": "pwgje,pwghf,trigger",
+    },
+    15: {"title": "[PWGJE,Trigger] :  Fix scope of tables in Jet.h", "labels": "pwgje,trigger"},
 }
 
-case = 0
+case = 15
 
 labels = cases[case]["labels"]
 title = cases[case]["title"]
 
 # This goes in the GitHub action.
 
-# import os
-# import re
-# import sys
-# title = os.environ['title']
-# labels = os.environ['labels']
+import os
+import re
+import sys
+
+# title = os.environ["title"]
+# labels = os.environ["labels"]
 tags = {
     "infrastructure": "Infrastructure",
+    "scripts": "Scripts",
+    "tools": "Tools",
     "common": "Common",
+    "trigger": "Trigger",
+    "dpg": "DPG",
+    "tutorial": "Tutorial",
     "alice3": "ALICE3",
     "pwgcf": "PWGCF",
     "pwgdq": "PWGDQ",
@@ -44,10 +57,8 @@ tags = {
     "pwghf": "PWGHF",
     "pwgje": "PWGJE",
     "pwglf": "PWGLF",
+    "pwgmm": "PWGMM",
     "pwgud": "PWGUD",
-    "dpg": "DPG",
-    "trigger": "Trigger",
-    "tutorial": "Tutorial",
 }
 print(f'PR title: "{title}"')
 print(f'PR labels: "{labels}"')
@@ -57,17 +68,15 @@ passed = True
 prefix_good = ",".join(tags_relevant)
 prefix_good = f"[{prefix_good}] "
 print(f"Generated prefix: {prefix_good}")
-replace_title = False
+replace_title = 0
 title_new = title
-# - If there is a prefix which contains a known tag:
-#   - Check it for correct tags and throw an error if extra/missing known tags are found.
-#   - Reformat a correct prefix if needed. Format: `[tag1,tag2,...] title`.
-# - If there is a prefix which does not contain any known tag, add the prefix with known tags.
-# - If there is no prefix, add the prefix with known tags.
-if match := re.match(r"\[?(\w[\w, /\+-]+)[\]:]+ ", title):
+# If there is a prefix which contains a known tag, check it for correct tags, and reformat it if needed.
+# If there is a prefix which does not contain any known tag, add the tag prefix.
+# If there is no prefix, add the tag prefix.
+if match := re.match(r" *\[?(\w[\w, /\+-]+)[\]:]+ ", title):
     prefix_title = match.group(1)
     words_prefix_title = prefix_title.replace(",", " ").replace("/", " ").split()
-    title_stripped = title[len(match.group()) :]
+    title_stripped = title[len(match.group()) :].strip()
     print(f'PR title prefix: "{prefix_title}" -> tags: {words_prefix_title}')
     print(f'Stripped PR title: "{title_stripped}"')
     if any(tag in words_prefix_title for tag in tags.values()):
@@ -82,29 +91,35 @@ if match := re.match(r"\[?(\w[\w, /\+-]+)[\]:]+ ", title):
         if passed:
             prefix_good = ",".join(w for w in prefix_title.replace(",", " ").split() if w)
             prefix_good = f"[{prefix_good}] "
-            print(f"::notice::Reformatted prefix: {prefix_good}")
-            if match.group() != prefix_good:
-                replace_title = True
+            print(f'::notice::Reformatted prefix: "{prefix_good}"')
+            if match.group() != prefix_good or title[len(match.group()) :] != title_stripped:
+                replace_title = 1
                 title_new = prefix_good + title_stripped
     else:
         print("::warning::No known tags found in the prefix.")
         if tags_relevant:
-            replace_title = True
-            title_new = prefix_good + title
+            replace_title = 1
+            title_new = prefix_good + title.strip()
+        elif title.strip() != title:
+            replace_title = 1
+            title_new = title.strip()
 else:
     print("::warning::No valid prefix found in the PR title.")
     if tags_relevant:
-        replace_title = True
-        title_new = prefix_good + title
+        replace_title = 1
+        title_new = prefix_good + title.strip()
+    elif title.strip() != title:
+        replace_title = 1
+        title_new = title.strip()
 if not passed:
     print("::error::Problems were found in the PR title prefix.")
     print('::notice::Use the form "tags: title" or "[tags] title".')
     sys.exit(1)
 if replace_title:
-    print("::warning::The PR title prefix with tags needs to be added or adjusted.")
+    print("::warning::The PR title needs to be adjusted.")
     print(f'::warning::New title: "{title_new}".')
 else:
-    print("::notice::The PR title prefix is fine.")
+    print("::notice::The PR title is fine.")
 # with open(os.environ["GITHUB_OUTPUT"], "a", encoding="utf-8") as fh:
 #     print(f"replace={replace_title}", file=fh)
 #     print(f"title={title_new}", file=fh)
