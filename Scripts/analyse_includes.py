4ed63c31e9f59be9beba8ff8c3d987454fd4d5a6
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2026-01-21 16:52:47 +0100

    Rewrite include analyser

--- Scripts/analyse_includes.py
+++ Scripts/analyse_includes.py
@@ -17,59 +17,204 @@
 @date   2025-12-09
 """
 
+# Covered cases:
+
+# Path is expl. relative. -> Explicit relative path
+# Path is expl. relative and does not end with ".h" -> Suspicious suffix
+# Path is expl. relative and does not match end of any absolute path. -> Not found
+# Path is not expl. relative and does not match end of any absolute path. -> OK (external)
+# Path matches an absolute path. -> OK (valid absolute path)
+
+# Path matches a relative path in the same directory. -> OK (valid relative path)
+# Path prefixed with "/include/" matches end of one absolute path. -> OK (special include directory)
+# Path matches a relative path in a higher parent directory. -> Incomplete relative path
+# Path is expl. relative and does not match a relative path in any parent directory. -> Wrong relative path
+# Path is not expl. relative and matches end of one absolute path. -> Incomplete absolute path
+
+# Path matches end of multiple absolute paths.
+#   -> There are multiple closest absolute paths. -> Ambiguous path
+#   -> There is one closest absolute path. -> Incomplete absolute path
+
 from pathlib import Path
 import re
 import sys
+import os
 
 
 if len(sys.argv) == 1:
     raise IOError("Provide a project base path.")
 
-dir_base = Path(sys.argv[1])
-if not dir_base.is_dir():
-    raise IOError("Provided path does not exist.")
 
-path_dir_base = str(dir_base) + "/"
-headers_local = dict.fromkeys(
-    [str(path).removeprefix(path_dir_base) for path in dir_base.rglob("*.h") if not path.name.endswith("LinkDef.h")], 0
-)
+class Resolver:
+    def __init__(self, path_base: str):
+        self.dir_base = Path(path_base)
+        if not self.dir_base.is_dir():
+            raise IOError("Provided path does not exist.")
+        self.path_dir_base = str(self.dir_base) + "/"
+        self.path_dir_base_absolute = str(self.dir_base.resolve()) + "/"
+        self.headers_local = dict.fromkeys(
+            [
+                str(path).removeprefix(self.path_dir_base)
+                for path in self.dir_base.rglob("*.h")
+                if not path.name.endswith("LinkDef.h")
+            ],
+            0,
+        )
+        self.path: Path = Path()
+        self.path_local: str = ""
+        self.location: str = ""
+        self.header: str = ""
+        self.style: str = ""
+        self.is_local: bool = False
+        self.is_found: bool = False
 
-for path in dir_base.rglob("*"):
-    if not re.search(r"^.+\.(h|cxx|cu|c|C)$", str(path)):
-        continue
-    with path.open() as file:
-        for i_line, line in enumerate(file.readlines()):
-            if not (match := re.match(r"#include ([\"<])(.+)[\">]", line)):
-                continue
-            location = f"{path}:{i_line + 1}"
-            header = match.group(2)
-            style = match.group(1)
-            is_local = False
-            if header in headers_local:
-                # print(f"{location}: Known header: {header}")
-                headers_local[header] += 1
-                is_local = True
+    def __check_format(self) -> None:
+        if self.is_local and self.style == "<":
+            print(f"{self.location}: Wrong include format for project header {self.header} [wrong-format-local]")
+            return
+        if not self.is_local and self.style == '"':
+            print(f"{self.location}: Wrong include format for external header {self.header} [wrong-format-external]")
+
+    def __check_unused(self) -> None:
+        for header, count in self.headers_local.items():
+            if count == 0:
+                print(f"{header}:0: Unused header [unused]")
+
+    def __process_include(self) -> None:
+        # Check the explicit relative path.
+        header_stripped = self.header
+        is_explicit_relative = False
+        if self.header.startswith("."):
+            print(f"{self.location}: Explicit relative path {self.header} [explicit-relative]")
+            if not self.header.endswith(".h"):
+                print(f"{self.location}: Not a header suffix in {self.header} [suspicious-suffix]")
+                return
+            self.is_local = True
+            is_explicit_relative = True
+            # Strip any relative path made of "." and "/".
+            header_stripped = self.header.lstrip("./")
+
+        # Find all matching headers.
+        candidates_h_local = [h_local for h_local in self.headers_local if h_local.endswith(f"/{header_stripped}")]
+        n_candidates = len(candidates_h_local)
+
+        if n_candidates == 0:
+            if is_explicit_relative:
+                print(f"{self.location}: Failed to find {self.header} [not-found]")
+            # else:
+            #     print(f"{self.location}: External header {self.header} [ok-external]")
+            return
+
+        self.is_local = True
+
+        # Check the absolute path.
+        if self.header in self.headers_local:
+            # print(f"{self.location}: Known header: {self.header}")
+            self.headers_local[self.header] += 1
+            print(f"{self.location}: Valid absolute path {self.header} [ok-absolute")
+            self.is_found = True
+            return
+
+        # Check the relative path w.r.t. the same directory.
+        if is_explicit_relative:
+            h_local = str((self.path.parent / self.header).resolve()).removeprefix(self.path_dir_base_absolute)
+        else:
+            h_local = str(self.path.parent / self.header).removeprefix(self.path_dir_base)
+        if h_local in self.headers_local:
+            self.headers_local[h_local] += 1
+            print(f"{self.location}: Valid relative path {self.header} to {h_local} [ok-relative]")
+            self.is_found = True
+            return
+
+        # Check for a path in a special "include" directory.
+        if not is_explicit_relative:
+            candidates_h_local_special = [
+                h_local for h_local in candidates_h_local if h_local.endswith(f"/include/{self.header}")
+            ]
+            if len(candidates_h_local_special) == 1:
+                h_local = candidates_h_local_special[0]
+                print(
+                    f"{self.location}: Valid relative path {self.header} to {h_local} in a special directory [ok-special]"
+                )
+                self.headers_local[h_local] += 1
+                self.is_found = True
+                return
+
+        # Check the relative path w.r.t. parent directories.
+        for directory in self.path.parent.parents:
+            if directory == self.path_dir_base:
+                break
+            if is_explicit_relative:
+                h_local = str((directory / self.header).resolve()).removeprefix(self.path_dir_base_absolute)
             else:
-                header_stripped = header
-                if header.startswith("."):
-                    print(f"{location}: Non-trivial relative path {header} [relative-path]")
-                    # Strip any relative path made of "." and "/".
-                    header_stripped = header.lstrip("./")
-                # Try to find a full header path matching the partial header path.
-                for h_local in headers_local:
-                    if h_local.endswith(f"/{header_stripped}"):
-                        is_local = True
-                        headers_local[h_local] += 1
-                        # Report path as incomplete, if the header is neither in the same directory nor in a special "include" directory.
-                        if str(path.parent / header).removeprefix(path_dir_base) != h_local and not h_local.endswith(f"/include/{header_stripped}"):
-                            print(f"{location}: Incomplete path {header} to {h_local} [incomplete-path]")
-                        break
-            if is_local and style == "<":
-                print(f"{location}: Wrong include format for project header {header} [wrong-format-local]")
-                continue
-            if not is_local and style == '"':
-                print(f"{location}: Wrong include format for external header {header} [wrong-format-external]")
+                h_local = str(directory / self.header).removeprefix(self.path_dir_base)
+            if h_local in candidates_h_local:
+                print(
+                    f"{self.location}: Incomplete relative path {self.header} to {h_local} [incomplete-relative-path]"
+                )
+                self.headers_local[h_local] += 1
+                self.is_found = True
+                return
+        if is_explicit_relative:
+            print(f"{self.location}: Wrong relative path {self.header} to {h_local} [wrong-relative-path]")
+            return
 
-for header, count in headers_local.items():
-    if count == 0:
-        print(f"{header}:0: Unused header [unused]")
+        # Incomplete absolute path.
+        if n_candidates == 1:
+            h_local = candidates_h_local[0]
+            print(f"{self.location}: Incomplete absolute path {self.header} to {h_local} [incomplete-absolute-path]")
+            self.headers_local[h_local] += 1
+            self.is_found = True
+            return
+
+        # Handle multiple path candidates.
+        if n_candidates > 1:
+            print(
+                f"{self.location}: Found multiple candidates for {self.header}: {candidates_h_local} [multiple-candidates]"
+            )
+            # Find the header with the longest common path.
+            len_prefix_max = 0
+            len_prefix_all = []
+            index_prefix_max = 0
+            for i, candidate in enumerate(candidates_h_local):
+                len_i = len(os.path.commonpath((candidate, self.path_local)))
+                len_prefix_all.append(len_i)
+                if len_i > len_prefix_max:
+                    len_prefix_max = len_i
+                    index_prefix_max = i
+            if (
+                len_prefix_max == 0 or len_prefix_all.count(len_prefix_max) > 1
+            ):  # no common prefix or several longest of same length
+                print(f"{self.location}: Ambiguous path {self.header} [ambiguous]")
+                return
+            h_local = candidates_h_local[index_prefix_max]
+            print(f"{self.location}: Best candidate for {self.header}: {h_local} [best-candidate]")
+            print(f"{self.location}: Incomplete absolute path {self.header} to {h_local} [incomplete-absolute-path]")
+            self.headers_local[h_local] += 1
+            self.is_found = True
+            return
+
+        print(f"{self.location}: Unhandled case {self.header} [error-exception]")
+
+    def run(self) -> None:
+        for path in self.dir_base.rglob("*"):
+            if not re.search(r"^.+\.(h|cxx|cu|c|C)$", str(path)):
+                continue
+            self.path = path
+            self.path_local = str(path).removeprefix(self.path_dir_base)
+            with self.path.open(encoding="utf-8") as file:
+                for i_line, line in enumerate(file.readlines()):
+                    if not (match := re.match(r"#include ([\"<])(.+)[\">]", line)):
+                        continue
+                    self.location = f"{path}:{i_line + 1}"
+                    self.header = match.group(2)
+                    self.style = match.group(1)
+                    self.is_local = False
+                    self.is_found = False
+                    self.__process_include()
+                    self.__check_format()
+        self.__check_unused()
+
+
+resolver = Resolver(sys.argv[1])
+resolver.run()
