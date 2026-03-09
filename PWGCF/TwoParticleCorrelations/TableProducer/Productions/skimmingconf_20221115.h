159b99eb0cdb615e0ae2a480b4972688eb6403a3
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2026-03-09 16:12:18 +0100

    Remove guard from special header

--- PWGCF/TwoParticleCorrelations/TableProducer/Productions/skimmingconf_20221115.h
+++ PWGCF/TwoParticleCorrelations/TableProducer/Productions/skimmingconf_20221115.h
@@ -9,8 +9,8 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#ifndef PWGCF_TWOPARTICLECORRELATIONS_TABLEPRODUCER_PRODUCTIONS_SKIMMINGCONF_20221115_H_
-#define PWGCF_TWOPARTICLECORRELATIONS_TABLEPRODUCER_PRODUCTIONS_SKIMMINGCONF_20221115_H_
+/// IMPORTANT: Cannot have header guard because it's included multiple times inside the code.
+// NOLINT(build/header_guard)
 
 #include <Framework/Configurable.h>
 
@@ -69,5 +69,3 @@ struct : o2::framework::ConfigurableGroup {
   o2::framework::Configurable<std::string> ccdbpath{"ccdbpath", "Users/v/victor/Skimming", "url of the skimming ccdb repository"};
   o2::framework::Configurable<std::string> filterdate{"filterdate", "20221115", "the date for the skimming production with the current filter configuration"};
 } filterccdb;
-
-#endif // PWGCF_TWOPARTICLECORRELATIONS_TABLEPRODUCER_PRODUCTIONS_SKIMMINGCONF_20221115_H_
