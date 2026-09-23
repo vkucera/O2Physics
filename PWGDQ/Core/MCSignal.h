aace266865f1bc16e4e734fa5c0b642959ff6440
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2026-09-23 17:05:20 +0200

    Fix readability/multiline_comment

--- PWGDQ/Core/MCSignal.h
+++ PWGDQ/Core/MCSignal.h
@@ -331,25 +331,26 @@ bool MCSignal::CheckProng(int i, bool checkSources, const T& track)
             break;
           }
         }
-      } /*else { // check generation in time
-        if (!currentMCParticle.has_daughters()) {
-          return false;
-        }
-        const auto& daughtersSlice = currentMCParticle.template daughters_as<P>();
-        for (auto& d : daughtersSlice) {
-          if (!fProngs[i].fExcludePDGInHistory[k] && fProngs[i].ComparePDG(d.pdgCode(), fProngs[i].fPDGInHistory[k], true, fProngs[i].fExcludePDGInHistory[k])) {
-            pdgInHistory.emplace_back(d.pdgCode());
-            break;
-          }
-          if (fProngs[i].fExcludePDGInHistory[k] && !fProngs[i].ComparePDG(d.pdgCode(), fProngs[i].fPDGInHistory[k], true, fProngs[i].fExcludePDGInHistory[k])) {
-            return false;
-          }
-          ith++;
-          if (ith > 10) { // need error message. Given pdg code was not found within 10 generations of the particles decay chain.
-            break;
-          }
-        }
-      }*/
+      }
+      // else { // check generation in time
+      //   if (!currentMCParticle.has_daughters()) {
+      //     return false;
+      //   }
+      //   const auto& daughtersSlice = currentMCParticle.template daughters_as<P>();
+      //   for (auto& d : daughtersSlice) {
+      //     if (!fProngs[i].fExcludePDGInHistory[k] && fProngs[i].ComparePDG(d.pdgCode(), fProngs[i].fPDGInHistory[k], true, fProngs[i].fExcludePDGInHistory[k])) {
+      //       pdgInHistory.emplace_back(d.pdgCode());
+      //       break;
+      //     }
+      //     if (fProngs[i].fExcludePDGInHistory[k] && !fProngs[i].ComparePDG(d.pdgCode(), fProngs[i].fPDGInHistory[k], true, fProngs[i].fExcludePDGInHistory[k])) {
+      //       return false;
+      //     }
+      //     ith++;
+      //     if (ith > 10) { // need error message. Given pdg code was not found within 10 generations of the particles decay chain.
+      //       break;
+      //     }
+      //   }
+      // }
     }
     if (pdgInHistory.size() != nIncludedPDG) { // vector has as many entries as mothers (daughters) defined for prong
       return false;
--- PWGLF/TableProducer/Resonances/cksspinalignment.cxx
+++ PWGLF/TableProducer/Resonances/cksspinalignment.cxx
@@ -139,11 +139,10 @@ struct cksspinalignment {
   }
 
   bool keepEvent(uint64_t eventIndex) const
-  { /*
-     if (cfgKeepFraction >= 1.0f) {
-       return true;
-     }
-    */
+  {
+    // if (cfgKeepFraction >= 1.0f) {
+    //   return true;
+    // }
     // SplitMix64 pseudo-random hash
     uint64_t x = eventIndex + 0x9e3779b97f4a7c15ULL;
     x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
